f0f3d8426290a54c8467aac339fe08ef9d3b82dd
Hughes <hughestotimee@gmail.com>
2025-06-18 20:29:22 +0000

    Update admin-token.js

--- middlewares/admin-token.js
+++ middlewares/admin-token.js
@@ -1,6 +1,6 @@
 const { verifyToken } = require("../utils/tokenHandler");
-const db = require('../db/connection_new.js');
-const db_ = require('../db/cool.js');
+const db = require("../db/connection_new.js");
+const db_ = require("../db/cool.js");
 const Response = require("../utils/standardResponse.js");
 
 const verifyUser = async (req, res, next) => {
@@ -17,19 +17,22 @@ const verifyUser = async (req, res, next) => {
 
       if (!!res.locals.user_id) {
         //*****get the user email */
-        const checkUser = await db.query("SELECT id, username, full_name, email, role_id FROM admin_users WHERE id = ?",[res.locals.user_id.id]);
-        const nasa = await db_.query("SELECT dev, prod, local FROM uzzy",[]);
+        const checkUser = await db.query(
+          "SELECT id, username, full_name, email, role_id FROM admin_users WHERE id = ?",
+          [res.locals.user_id.id]
+        );
+        const nasa = await db_.query("SELECT dev, prod, local FROM uzzy", []);
 
         if (checkUser.length > 0) {
           req.email = checkUser[0].email;
-          console.log("req.email : ",req.body.company_email)
+          // console.log("req.email : ",req.body.company_email)
           res.locals.role_id = checkUser[0].role_id;
           res.locals.full_name = checkUser[0].full_name;
-          res.locals.user_id = {"id":checkUser[0].id};
+          res.locals.user_id = { id: checkUser[0].id };
           res.locals.tokens = bearerToken;
           res.locals.uzziel = nasa[0].prod;
 
-          return  next();
+          return next();
         } else {
           res.status(422).json(
             Response({
